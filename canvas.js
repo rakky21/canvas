5c36a7c34f8a738c787e7d4491485334cd1ce140
fcribas <rakky26@gmail.com>
2022-10-11 13:14:56 -0400

    displaued multiple circles and changed the speed

--- canvas.js
+++ canvas.js
@@ -5,28 +5,56 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext("2d");
 
-var x = Math.random() * innerWidth;
-var y = Math.random() * innerHeight;
-var dx = Math.random() - 0.5 * 4; 
-var dy = Math.random() - 0.5 * 4;
-var radius = 30;
+console.log('Hello')
+
+function Circle(x, y, dx, dy, radius) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+
+  this.draw = function () {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = "blue";
+    c.stroke();
+    c.fill();
+  };
+
+  this.update = function () {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+    this.draw();
+  };
+}
+
+//FOR LOPP
+
+var circleArray = [];
+
+for (var i = 0; i < 1000; i++) {
+  var radius = 30;
+  var x = Math.random() * (innerWidth - radius * 4) + radius;
+  var y = Math.random() * (innerHeight - radius * 4) + radius;
+  var dx = Math.random() - 12;
+  var dy = Math.random() - 12;
+  circleArray.push(new Circle(x, y, dx, dy, radius));
+}
 
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
-  c.beginPath();
-  c.arc(x, y, radius, 0, Math.PI * 2, false);
-  c.strokeStyle = "blue";
-  c.stroke();
-
-  if (x + radius > innerWidth || x - radius < 0) {
-    dx = -dx;
+  for (var i = 0; i < circleArray.length; i++) {
+    circleArray[i].update();
   }
-  if (y + radius > innerHeight || y - radius < 0) {
-    dy = -dy;
-  }
-  x += dx;
-  y += dy;
+  circle.update();
 }
 animate();
 
